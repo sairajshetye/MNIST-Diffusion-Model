46db085c540ae58023806f73609df25078c2b479
haunters <hauntersgd@gmail.com>
2024-12-03 07:03:52 -0600

    part 3 not working

--- networks.py
+++ networks.py
@@ -37,16 +37,62 @@ class SimpleDecoder(nn.Module):
         return self.decoder(x)
 
 
+class ContractionBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.contract = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),  # possibly double number of channels here
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # halve size of image
+        )
+    
+    def forward(self, x):
+        return self.contract(x)
+
+class ExpansionBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.expand = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),  # possibly halve number of channels here
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),  
+            nn.ReLU(),
+            nn.ConvTranspose2d(out_channels, out_channels, kernel_size=2, stride=2),  # double size of image
+        )
+    
+    def forward(self, x):
+        return self.expand(x)
+
 
 class UNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=1, out_channels = 1, image_width=32, image_height=32):
         super().__init__()
         # TODO (student): If you want to use a UNet, you may use this class
+        
+        self.contractor = nn.Sequential(       # 32 x 32
+            ContractionBlock(in_channels, 64), # 16 x 16
+            ContractionBlock(64, 128),        # 8 x 8
+            ContractionBlock(128, 256)         # 4 X 4
+        )
+        
+        self.expander = nn.Sequential(       # 32 x 32
+            ExpansionBlock(256, 128), # 16 x 16
+            ExpansionBlock(128, 64),        # 8 x 8
+            ExpansionBlock(64, out_channels),         # 4 X 4
+        )
+        
     
     def forward(self, inputs):
         batch = inputs.shape[0]
         inputs = inputs.reshape(batch, 1, 32, 32)
         outputs = inputs
+        
         # TODO (student): If you want to use a UNet, you may use this class
+        outputs = self.contractor(outputs)
+        outputs = self.expander(outputs)
+        
+        
         outputs = outputs.reshape(batch, -1)
         return outputs
